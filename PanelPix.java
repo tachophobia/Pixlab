386b10c41fd169d0abae779ad581bfc355d26004
Nikita Lugovoy <niklugovoy@gmail.com>
2023-04-18 21:03:15 -0400

    first commit

---
+++ PanelPix.java
@@ -0,0 +1,321 @@
+
+//
+//name:        date:
+//
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+//
+public class PanelPix extends JPanel {
+
+   private DisplayPix display;
+
+   private ScoreboardPix scoreboard;
+
+   public PanelPix() {
+      setLayout(new BorderLayout());
+      //
+      // north
+      //
+      scoreboard = new ScoreboardPix();
+      add(scoreboard, BorderLayout.NORTH);
+      //
+      // east
+      //
+      JPanel east = new JPanel();
+      east.setLayout(new GridLayout(18, 1));
+      /****************************************************************** 1 */
+      JButton zero = new JButton("Zero Blue");
+      zero.addActionListener(new Listener_zeroBlue());
+      east.add(zero);
+      /****************************************************************** 2 */
+      JButton negate = new JButton("Negate");
+      negate.addActionListener(new Listener_negate());
+      east.add(negate);
+      /****************************************************************** 3 */
+      JButton gray = new JButton("Grayscale");
+      gray.addActionListener(new Listener_grayScale());
+      east.add(gray);
+      /****************************************************************** 4 */
+      JButton sepia = new JButton("Sepia Tone");
+      sepia.addActionListener(new Listener_sepiaTone());
+      east.add(sepia);
+      /****************************************************************** 5 */
+      JButton blur = new JButton("Blur");
+      blur.addActionListener(new Listener_blur());
+      east.add(blur);
+      /****************************************************************** 6 */
+      JButton posterize = new JButton("Posterize");
+      posterize.addActionListener(new Listener_posterize());
+      east.add(posterize);
+      /****************************************************************** 7 */
+      JButton splash = new JButton("Color Splash");
+      splash.addActionListener(new Listener_colorSplash());
+      east.add(splash);
+      /****************************************************************** 8 */
+      JButton mirrorLR = new JButton("Mirror Left-Right");
+      mirrorLR.addActionListener(new Listener_mirrorLR());
+      east.add(mirrorLR);
+      /****************************************************************** 9 */
+      JButton mirrorUD = new JButton("Mirror Up-Down");
+      mirrorUD.addActionListener(new Listener_mirrorUD());
+      east.add(mirrorUD);
+      /****************************************************************** 10 */
+      JButton flipLR = new JButton("Flip Left-Right");
+      flipLR.addActionListener(new Listener_flipLR());
+      east.add(flipLR);
+      /****************************************************************** 11 */
+      JButton flipUD = new JButton("Flip Up-Down");
+      flipUD.addActionListener(new Listener_flipUD());
+      east.add(flipUD);
+      /****************************************************************** 12 */
+      JButton pixelate = new JButton("Pixelate");
+      pixelate.addActionListener(new Listener_pixelate());
+      east.add(pixelate);
+      /****************************************************************** 13 */
+      JButton sunsetize = new JButton("Sunsetize");
+      sunsetize.addActionListener(new Listener_sunsetize());
+      east.add(sunsetize);
+      /****************************************************************** 14 */
+      JButton redeye = new JButton("Remove Red-Eye");
+      redeye.addActionListener(new Listener_redeye());
+      east.add(redeye);
+      /****************************************************************** 15 */
+      JButton detect = new JButton("Edge Detector");
+      detect.addActionListener(new Listener_detect());
+      east.add(detect);
+      /****************************************************************** 16a */
+      JButton encoder = new JButton("Encode");
+      encoder.addActionListener(new Listener_encode());
+      east.add(encoder);
+      /****************************************************************** 16b */
+      JButton decoder = new JButton("Decode");
+      decoder.addActionListener(new Listener_decode());
+      east.add(decoder);
+
+      /****************************************************************** 17 */
+      JButton chromakey = new JButton("Chromakey");
+      chromakey.addActionListener(new Listener_chromakey());
+      east.add(chromakey);
+
+      /****************************************************************** END */
+      add(east, BorderLayout.EAST);
+      //
+      // center
+      //
+      display = new DisplayPix();
+      display.addMouseListener(new Mouse());
+
+      display.addKeyListener(new Key());
+      display.setFocusable(true);
+      add(display, BorderLayout.CENTER);
+      //
+      // south
+      //
+      JPanel south = new JPanel();
+      south.setLayout(new FlowLayout());
+      JButton restore = new JButton("Restore Original Image");
+      restore.addActionListener(new Listener_restore());
+      south.add(restore);
+      JButton openimg = new JButton("Open an Image File");
+      openimg.addActionListener(new Listener_openimg());
+      south.add(openimg);
+      JButton undo = new JButton("Undo");
+      undo.setEnabled(false);
+      south.add(undo);
+      add(south, BorderLayout.SOUTH);
+   }
+
+   //
+   /**********************************************************************/
+   //
+   // pixel operation listener
+   //
+   private class Listener_zeroBlue implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.zeroBlue();
+         update(display.getXval(), display.getYval());
+      }
+   }
+   // ----------> add more Listeners here <-----------
+
+   private class Listener_negate implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.negate();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_grayScale implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.grayScale();
+         update(display.getXval(), display.getYval());
+      }
+
+   }
+
+   private class Listener_sepiaTone implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.sepiaTone();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_blur implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.blur();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_posterize implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.posterize();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_colorSplash implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.colorSplash();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_mirrorLR implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.mirrorLR();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_mirrorUD implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.mirrorUD();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_flipLR implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.flipLR();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_flipUD implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.flipUD();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_pixelate implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.pixelate();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_sunsetize implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.sunsetize();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_redeye implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.redeye();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_detect implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.detect();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_encode implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.encode();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_decode implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.decode();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_chromakey implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.chromakey();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   //
+   /**********************************************************************/
+   //
+   private class Listener_restore implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         display.resetImage();
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private class Listener_openimg implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         if (display.openImage()) {
+            update(display.getXval(), display.getYval());
+         }
+      }
+   }
+
+   private class Mouse extends MouseAdapter {
+      public void mouseClicked(MouseEvent e) {
+         update(e.getX(), e.getY());
+      }
+   }
+
+   private class Key extends KeyAdapter {
+      public void keyPressed(KeyEvent e) {
+         switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+               display.up();
+               break;
+            case KeyEvent.VK_DOWN:
+               display.down();
+               break;
+            case KeyEvent.VK_LEFT:
+               display.left();
+               break;
+            case KeyEvent.VK_RIGHT:
+               display.right();
+               break;
+         }
+         //
+         update(display.getXval(), display.getYval());
+      }
+   }
+
+   private void update(int x, int y) {
+      int rgb = display.getRGB(x, y);
+      //
+      display.update(x, y);
+      scoreboard.update(display.getCol(), display.getRow(), rgb);
+      //
+      display.repaint();
+      //
+      display.requestFocus();
+   }
+}
+//
+// end of file
+//
---
+++ PixelOperations.java
@@ -0,0 +1,236 @@
+
+//
+// Torbert, 24 July 2013
+//
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.swing.JOptionPane;
+
+//
+public class PixelOperations {
+   public Color[][] getArray(BufferedImage img) {
+      Color[][] arr;
+      //
+      int numcols = img.getWidth();
+      int numrows = img.getHeight();
+      //
+      arr = new Color[numrows][numcols];
+      //
+      for (int j = 0; j < arr.length; j++) {
+         for (int k = 0; k < arr[0].length; k++) {
+            int rgb = img.getRGB(k, j);
+            //
+            arr[j][k] = new Color(rgb);
+         }
+      }
+      //
+      return arr;
+   }
+
+   public void setImage(BufferedImage img, Color[][] arr) {
+      for (int j = 0; j < arr.length; j++) {
+         for (int k = 0; k < arr[0].length; k++) {
+            Color tmp = arr[j][k];
+            //
+            int rgb = tmp.getRGB();
+            //
+            img.setRGB(k, j, rgb);
+         }
+      }
+   }
+
+   //
+   /**********************************************************************/
+   //
+   // pixel operations
+   //
+   public void zeroBlue(Color[][] arr) {
+      for (int j = 0; j < arr.length; j++) {
+         for (int k = 0; k < arr[0].length; k++) {
+            Color tmp = arr[j][k];
+            arr[j][k] = new Color(tmp.getRed(), tmp.getGreen(), 0);
+         }
+      }
+   }
+
+   // --------> your new methods go here <--------------
+   public void negate(Color[][] arr) {
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = 0; j < arr[i].length; j++) {
+            Color tmp = arr[i][j];
+            arr[i][j] = new Color(255 - tmp.getRed(), 255 - tmp.getGreen(), 255 - tmp.getBlue());
+         }
+      }
+   }
+
+   public void grayScale(Color[][] arr) {
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = 0; j < arr[i].length; j++) {
+            Color tmp = arr[i][j];
+            int avg = (tmp.getRed() + tmp.getGreen() + tmp.getBlue()) / 3;
+            arr[i][j] = new Color(avg, avg, avg);
+         }
+      }
+   }
+
+   public void sepiaTone(Color[][] arr) {
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = 0; j < arr[i].length; j++) {
+            Color tmp = arr[i][j];
+
+            int r = (int) (.393 * tmp.getRed() + .769 * tmp.getGreen() + .189 * tmp.getBlue());
+            int g = (int) (.349 * tmp.getRed() + .686 * tmp.getGreen() + .168 * tmp.getBlue());
+            int b = (int) (.272 * tmp.getRed() + .534 * tmp.getGreen() + .131 * tmp.getBlue());
+
+            r = Math.min(255, r);
+            g = Math.min(255, g);
+            b = Math.min(255, b);
+
+            arr[i][j] = new Color(r, g, b);
+         }
+      }
+   }
+
+   public void blur(Color[][] arr) {
+      // get the colors of the four pixels to the left, right, bottom, and top of the
+      // current pixel.
+      // set the current pixel's color to the average of the colors of the current
+      // pixel and its four neighboring pixels.
+      // ignore screen edges
+
+      int n = 10; // amount of blur
+      while (n > 0) {
+         n--;
+         for (int i = 1; i < arr.length - 1; i++) {
+            for (int j = 1; j < arr[i].length - 1; j++) {
+               Color top = arr[i - 1][j];
+               Color bottom = arr[i + 1][j];
+               Color left = arr[i][j - 1];
+               Color right = arr[i][j + 1];
+
+               int r = (int) ((top.getRed() + bottom.getRed() + left.getRed() + right.getRed()) / 4);
+               int g = (int) ((top.getGreen() + bottom.getGreen() + left.getGreen() + right.getGreen()) / 4);
+               int b = (int) ((top.getBlue() + bottom.getBlue() + left.getBlue() + right.getBlue()) / 4);
+
+               arr[i][j] = new Color(r, g, b);
+            }
+         }
+      }
+   }
+
+   public void posterize(Color[][] arr) {
+      // for a range of colors, map them to a single color.
+
+      // for each color if it is
+      // between 0 and 62; set to 31
+      // between 63 and 128; set to 95
+      // between 129 and 191; set to 159
+      // between 192 and 255; set to 223
+
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = 0; j < arr[i].length; j++) {
+            Color tmp = arr[i][j];
+            int rgb[] = { tmp.getRed(), tmp.getGreen(), tmp.getBlue() }; // you could initialize this array with
+                                                                         // int rgb[] = new int[] {...}
+                                                                         // but i think that isn't necessary anymore in
+                                                                         // newer versions of java
+            for (int k = 0; k < rgb.length; k++) {
+               if (rgb[k] < 63) {
+                  rgb[k] = 31;
+               } else if (rgb[k] < 128) {
+                  rgb[k] = 95;
+               } else if (rgb[k] < 192) {
+                  rgb[k] = 159;
+               } else {
+                  rgb[k] = 223;
+               }
+            }
+            arr[i][j] = new Color(rgb[0], rgb[1], rgb[2]);
+         }
+      }
+   }
+
+   public void colorSplash(Color[][] arr) {
+      // turn picture into a grayscale except for red colors
+      // a color is red if red component is greater than the sum of its green and blue
+
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = 0; j < arr[i].length; j++) {
+            Color tmp = arr[i][j];
+            int r = tmp.getRed();
+            int g = tmp.getGreen();
+            int b = tmp.getBlue();
+            int sum = r + g + b;
+            if (r > (sum - r)) {
+               arr[i][j] = new Color(255, sum / 3, sum / 3);
+            } else {
+               arr[i][j] = new Color(sum / 3, sum / 3, sum / 3);
+            }
+         }
+      }
+   }
+
+   public void mirrorLR(Color[][] arr) {
+      // mirror image horizontally, split in the center
+
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = 0; j < arr[i].length / 2; j++) {
+            Color tmp = arr[i][j];
+            arr[i][arr[i].length - 1 - j] = tmp;
+         }
+      }
+   }
+
+   public void mirrorUD(Color[][] arr) {
+      // mirror image vertically, split in the middle
+
+      for (int i = 0; i < arr.length / 2; i++) {
+         for (int j = 0; j < arr[i].length; j++) {
+            Color tmp = arr[i][j];
+            arr[arr.length - 1 - i][j] = tmp;
+         }
+      }
+   }
+
+   public void flipLR(Color[][] arr) {
+      // flip across y-axis
+
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = 0; j < arr[i].length / 2; j++) {
+            Color tmp = arr[i][j];
+            arr[i][j] = arr[i][arr[i].length - 1 - j];
+            arr[i][arr[i].length - 1 - j] = tmp;
+         }
+      }
+   }
+
+   public void flipUD(Color[][] arr) {
+      // flip across x-axis
+
+      for (int i = 0; i < arr.length / 2; i++) {
+         for (int j = 0; j < arr[i].length; j++) {
+            Color tmp = arr[i][j];
+            arr[i][j] = arr[arr.length - 1 - i][j];
+            arr[arr.length - 1 - i][j] = tmp;
+         }
+      }
+   }
+
+   public void pixelate(Color[][] arr) {
+      // prompt the user for the size of the pixelation
+
+      int size;
+      try {
+         size = Integer.parseInt(JOptionPane.showInputDialog("Enter the size of the pixelation"));
+      } catch (Exception e) {
+         JOptionPane.showMessageDialog(null, "Invalid input");
+         return;
+      }
+      // pixelate the image by the size of the pixelation
+   }
+
+}
+
+//
+// end of file
+//
